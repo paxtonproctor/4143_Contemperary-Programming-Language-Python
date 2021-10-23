68f6e34a88229c5c49d58bb79b4853e30a54e661
Paxton Proctor <pax.proctor@gmail.com>
2021-10-23 18:18:38 -0500

    change

---
+++ InClassAssignments/programAssignments/Program1_Problem1.py
@@ -0,0 +1,11 @@
+#Paxton Proctor
+# Programassignment1
+# CMPS-4143-101 Top: Cont Programming Language
+# 10/23/2021
+# Write a Python program to draw a three-stagerocket.
+
+var = '*--------*'
+var1 = '*        *\n'
+print(var)
+print(var1)
+

0c90a5c06036f1387180de1b5dcc784727248f85
Paxton Proctor <pax.proctor@gmail.com>
2021-10-26 20:22:43 -0500

    Rocket!!!!!

--- InClassAssignments/programAssignments/Program1_Problem1.py
+++ InClassAssignments/programAssignments/Program1_Problem1.py
@@ -1,11 +1,40 @@
-#Paxton Proctor
+# Paxton Proctor
 # Programassignment1
 # CMPS-4143-101 Top: Cont Programming Language
 # 10/23/2021
 # Write a Python program to draw a three-stagerocket.
 
-var = '*--------*'
-var1 = '*        *\n'
-print(var)
-print(var1)
+#
+def rocket_parts1():
+    print('       * ')
+    print('      * *')
+    print('     *   *')
 
+#
+def rocket_parts2():
+    print('     |===|')
+    print('     |   |')
+    print('     |   |')
+    print('     |   |')
+    print('     |   |')
+    print('     |===|')
+
+#
+def rocket_parts3():
+    print('     |\ /|')
+    print('     | X |')
+    print('     |/ \|')
+    print('     |===|')
+    print('   */|\ /|\*')
+    print('  */ | X | \*')
+    print(' */  |/ \|  \*')
+    print('*/===========\*')
+    print('      ***')
+    print('     *****')
+    print('      ***')
+    print('       *')
+
+# calling definitions
+rocket_parts1()
+rocket_parts2()
+rocket_parts3()

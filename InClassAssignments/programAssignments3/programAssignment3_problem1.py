33459694b12b0196c83c1df61ff1cee53550c61e
Paxton Proctor <pax.proctor@gmail.com>
2021-11-16 13:55:41 -0600

    problem 1 finished

--- InClassAssignments/programAssignments3/programAssignment3_problem1.py
+++ InClassAssignments/programAssignments3/programAssignment3_problem1.py
@@ -17,33 +17,42 @@ class Stack:
     # Constructor for head
     def __init__(self):
         self.head = None
+        self.min = None
     
 
     # add data to the start of the stack
     def push(self, data): 
         if self.head == None:
             self.head = Node(data)
+            print("Successfully pushed: ", self.head.data)
         else:
             newnode = Node(data)
             newnode.next = self.head
             self.head = newnode
+            print("Successfully pushed: ", self.head.data)
+        self.minimum()
     
+    # tracks minimum value
+    def minimum(self): 
+        if self.min is None:
+            self.min = self.top()
+        else:
+            if self.top() < self.min:
+                self.min = self.top()
 
-    #def minimum(self): 
-        #if self.min is None:
-            #self.min = self.viewtop()
-        #else:
-            #if self.viewtop() < self.min:
-                #self.min = self.viewtop()
+    # gets minimum
+    def Min(self): 
+        return self.min
 
-    #def Min(self): 
-        #return self.min
-
-    #def viewtop(self): 
-       # try:
-            #return self.data[-1]
-        #except IndexError as e:
-            #print(e)
+    # tries to check if top is empty else point to data
+    def top(self): 
+        try:
+            if self.isEmpty():
+                return None
+            else:
+                return self.head.data
+        except IndexError as e:
+            print(e)
 
     # removes element from start of the stack
     def pop(self): 
@@ -55,12 +64,20 @@ class Stack:
             poppednode = self.head
             self.head = self.head.next
             poppednode.next = None
-            print("popping element successful")
+            print("popping element successful: ", poppednode.data)
             return poppednode.data            
 
-
-    #def size(self): 
-        #return len(self.data)
+    # gets the Size of the stack
+    def size(self): 
+        iterateNode = self.head
+        if self.isEmpty():
+            print("Stack is empty")
+        else:
+            while(iterateNode != None):
+                print(iterateNode.data,"->", end = " ")
+                iterateNode = iterateNode.next
+            return
+        
 
     # checks to see if stack is empty
     def isEmpty(self): 
@@ -69,3 +86,29 @@ class Stack:
         else:
             return False
 
+NodeStack = Stack()
+
+NodeStack.push(12)
+NodeStack.push(13)
+NodeStack.push(18)
+NodeStack.push(23)
+NodeStack.push(43)
+NodeStack.push(34)
+
+NodeStack.size()
+
+print("\nTop of Stack is:", NodeStack.top())
+
+NodeStack.pop()
+NodeStack.pop()
+NodeStack.pop()
+
+print("here is the minimum after push: ",NodeStack.Min())
+
+print("Top of Stack is:", NodeStack.top())
+
+NodeStack.push(3)
+
+NodeStack.size()
+
+print("\nhere is the new minimum after push: ",NodeStack.Min())
